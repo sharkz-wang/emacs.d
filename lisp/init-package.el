66f6070181fee96b7b6eca29a0d63df2c1f9835e
Sharkz Wang <sharkz.wang@gmail.com>
2017-05-07 12:58:05 +0800

    added settings for package.el

---
+++ lisp/init-package.el
@@ -0,0 +1,22 @@
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives
+			 '("melpa" . "https://melpa.org/packages/"))
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (if (boundp 'package-selected-packages)
+            ;; Record this as a package the user installed explicitly
+            (package-install package nil)
+          (package-install package))
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(provide 'init-package)
