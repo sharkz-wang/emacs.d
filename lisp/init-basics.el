29b460880d778af4bcb14ee9d4163438d4b210ab
Sharkz Wang <sharkz.wang@gmail.com>
2017-05-08 22:24:16 +0800

    changed default font

--- lisp/init-basics.el
+++ lisp/init-basics.el
@@ -2,7 +2,7 @@
 (setq inhibit-splash-screen t)
 ;; display column numbers in mode-line
 (column-number-mode 1)
-(set-default-font "Deja Vu Sans Mono-16")
+(set-default-font "Monaco-16")
 ;; no tool bars
 (tool-bar-mode -1)
 ;; no memu bar
