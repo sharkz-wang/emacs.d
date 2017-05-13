d0267607b24f2ecefbd357002a051a4878238a55
Sharkz Wang <sharkz.wang@gmail.com>
2017-05-13 19:11:42 +0800

    added settings for smooth scrolling

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -6,4 +6,11 @@
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 (define-key evil-normal-state-map (kbd "SPC w") 'ace-window)
 
+(require-package 'smooth-scrolling)
+(smooth-scrolling-mode 1)
+
+(require-package 'sublimity)
+(require 'sublimity-scroll)
+(sublimity-mode 1)
+
 (provide 'init-navigation)
