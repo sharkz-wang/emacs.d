244dee02069fec18e20c1d8a6eaa6c191a886042
Sharkz Wang <sharkz.wang@gmail.com>
2017-05-14 23:18:49 +0800

    added settings for package vimish-fold

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -15,4 +15,18 @@
 (require 'sublimity-scroll)
 (sublimity-mode 1)
 
+(require-package 'vimish-fold)
+
+(defun vimish-action (arg) (interactive "P")
+       (if (equal current-prefix-arg '(4))
+	   (vimish-fold-delete)
+	 (vimish-fold-toggle)
+	 ))
+
+(define-key evil-normal-state-map (kbd "S-TAB") 'vimish-action)
+(define-key evil-normal-state-map (kbd "<backtab>") 'vimish-action)
+
+(define-key evil-visual-state-map (kbd "S-TAB") 'vimish-fold)
+(define-key evil-visual-state-map (kbd "<backtab>") 'vimish-fold)
+
 (provide 'init-navigation)
