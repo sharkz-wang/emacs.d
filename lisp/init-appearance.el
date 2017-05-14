b6d4e1d41326bf691d6942791bdae448c956ac79
Sharkz Wang <sharkz.wang@gmail.com>
2017-05-14 23:08:43 +0800

    added package moe-theme

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -1,8 +1,10 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (require-package 'monokai-theme)
-
 (load-theme 'monokai t)
+
+(require-package 'moe-theme)
+(require 'moe-theme)
 
 (require-package 'zone-rainbow)
 
