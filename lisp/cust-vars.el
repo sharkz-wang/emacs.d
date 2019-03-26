822233d6762693e845b837ef6144f51639cfb97b
Sharkz Wang <sharkz.wang@gmail.com>
2019-03-26 23:17:09 +0800

    tmp

--- lisp/cust-vars.el
+++ lisp/cust-vars.el
@@ -5,6 +5,8 @@
  '(plantuml-jar-path (expand-file-name "~/plantuml.jar"))
  '(pdf-info-epdfinfo-program "~/epdfinfo")
  '(sublimity-attractive-centering-width 90)
+ '(ycmd-server-command (list "python" (expand-file-name "~/ycmd/ycmd")))
+ '(ycmd-global-config "~/ycmd/cpp/ycm/.ycm_extra_conf.py")
  )
 
 (provide 'cust-vars)
