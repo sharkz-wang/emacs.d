872a0e10a82b93b3bbdbe342f7071d4d18965388
Sharkz Wang <sharkz.wang@gmail.com>
2019-06-20 22:11:40 +0800

    c++-lang: applied c-lang customizations to c++-lang

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,79 +1,81 @@
-(add-hook 'c-mode-hook
-	  (lambda ()
-	    
-	    (modify-syntax-entry ?_ "w")
-	    
-	    (custom-set-variables
-	     '(c-basic-offset 'set-from-style))
+(defun init-cc-handler ()
 
-	    ;; (require 'init-semantic)
-	    (require 'init-gtags)
-	    (helm-gtags-mode t)
-	    ;; (require 'init-company)
-	    ;; (require 'init-ycmd)
-	    
-	    ;; auto indenting and pairing curly brace
-	    (defun c-mode-insert-lcurly ()
-	      (interactive)
-	      (insert "{")
-	      (let ((pps (syntax-ppss)))
-		;; when EOL and not in string or comment
-		(when (and (eolp) (not (or (nth 3 pps) (nth 4 pps))))
-		  (c-indent-line)
-		  (insert "\n\n}")
-		  (c-indent-line)
-		  (forward-line -1)
-		  (c-indent-line))))
-	    (define-key c-mode-base-map "{" 'c-mode-insert-lcurly)
+  (modify-syntax-entry ?_ "w")
+  
+  (custom-set-variables
+   '(c-basic-offset 'set-from-style))
 
-	    (defun choose-c-style ()
-	      (interactive)
-	      (helm :sources
-		    (helm-build-sync-source "c styles"
-		      :candidates (mapcar 'car c-style-alist))))
+  ;; (require 'init-semantic)
+  (require 'init-gtags)
+  (helm-gtags-mode t)
+  ;; (require 'init-company)
+  ;; (require 'init-ycmd)
+  
+  ;; auto indenting and pairing curly brace
+  (defun c-mode-insert-lcurly ()
+    (interactive)
+    (insert "{")
+    (let ((pps (syntax-ppss)))
+      ;; when EOL and not in string or comment
+      (when (and (eolp) (not (or (nth 3 pps) (nth 4 pps))))
+	(c-indent-line)
+	(insert "\n\n}")
+	(c-indent-line)
+	(forward-line -1)
+	(c-indent-line))))
+  (define-key c-mode-base-map "{" 'c-mode-insert-lcurly)
 
-	    (defun change-c-style ()
-	      (interactive)
-	      (c-set-style (choose-c-style))
-	      (setq c-basic-offset 'set-from-style))
+  (defun choose-c-style ()
+    (interactive)
+    (helm :sources
+	  (helm-build-sync-source "c styles"
+	    :candidates (mapcar 'car c-style-alist))))
 
-	    (defun choose-c-offset ()
-	      (interactive)
-	      (helm :sources
-		    (helm-build-sync-source "c offset"
-		      :candidates '(2 4 8 "set-from-style"))))
+  (defun change-c-style ()
+    (interactive)
+    (c-set-style (choose-c-style))
+    (setq c-basic-offset 'set-from-style))
 
-	    (defun change-c-offset ()
-	      (interactive)
-	      (let ((indent (choose-c-offset)))
-		(if (string-equal indent "set-from-style")
-		    (progn
-		      (setq c-basic-offset 'set-from-style)
-		      (setq tab-width 8))
-		  (progn
-		    (setq c-basic-offset (string-to-number indent))
-		    (setq tab-width (string-to-number indent)))
-		  )))
+  (defun choose-c-offset ()
+    (interactive)
+    (helm :sources
+	  (helm-build-sync-source "c offset"
+	    :candidates '(2 4 8 "set-from-style"))))
 
-	    (defun toggle-indent-tabs-mode ()
-	      (interactive)
-	      (let ((indent (if indent-tabs-mode nil t)))
-		(setq indent-tabs-mode indent)
-		(message "toggle indent-tab-mode: %s" indent)))
+  (defun change-c-offset ()
+    (interactive)
+    (let ((indent (choose-c-offset)))
+      (if (string-equal indent "set-from-style")
+	  (progn
+	    (setq c-basic-offset 'set-from-style)
+	    (setq tab-width 8))
+	(progn
+	  (setq c-basic-offset (string-to-number indent))
+	  (setq tab-width (string-to-number indent)))
+	)))
 
-	    (defun c-style-selector ()
-	      (interactive)
-	      (eval (car (read-from-string
-			  (format "(%s)"
-				  (helm :sources
-					(helm-build-sync-source "c style setters"
-					  :candidates '(change-c-offset
-							change-c-style
-							toggle-indent-tabs-mode))))))))
+  (defun toggle-indent-tabs-mode ()
+    (interactive)
+    (let ((indent (if indent-tabs-mode nil t)))
+      (setq indent-tabs-mode indent)
+      (message "toggle indent-tab-mode: %s" indent)))
 
-	    (evil-leader/set-key
-	      "ms" 'c-style-selector
-	      )
-	    ))
+  (defun c-style-selector ()
+    (interactive)
+    (eval (car (read-from-string
+		(format "(%s)"
+			(helm :sources
+			      (helm-build-sync-source "c style setters"
+				:candidates '(change-c-offset
+					      change-c-style
+					      toggle-indent-tabs-mode))))))))
+
+  (evil-leader/set-key
+    "ms" 'c-style-selector
+    )
+  )
+
+(add-hook 'c-mode-hook 'init-cc-handler)
+(add-hook 'c++-mode-hook 'init-cc-handler)
 
 (provide 'init-c)
