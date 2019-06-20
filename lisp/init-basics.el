eaa28956cfc36b9086f806441581115eaa9129fc
Sharkz Wang <sharkz.wang@gmail.com>
2019-06-20 22:06:08 +0800

    basics: purged dead code

--- lisp/init-basics.el
+++ lisp/init-basics.el
@@ -59,17 +59,6 @@
 
 (require 'init-evil)
 
-(define-key evil-normal-state-map
-  (kbd "SPC TAB")
-  (lambda (arg) (interactive "P")
-    (if (equal current-prefix-arg '(4))
-	(progn
-	  (set-frame-parameter nil 'fullscreen 'maximized)
-	)
-      (set-frame-parameter nil 'fullscreen 'fullboth)
-      )
-    ))
-
 (defun switch-to-last-buffer ()
   "Switch to previously open buffer.
    Repeated invocations toggle between the two most recently open buffers."
