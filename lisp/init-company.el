bf1ed13c782ebb418a3f305d3f0efea3fd9a0af4
Sharkz Wang <sharkz.wang@gmail.com>
2020-06-20 17:26:32 +0800

    company: removed yas backend from company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -22,15 +22,6 @@
 
 (require-package 'yasnippet-snippets)
 
-(defvar company-mode/enable-yas t
-  "Enable yasnippet for all backends.")
-(defun company-mode/backend-with-yas (backend)
-  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-	  backend
-	(append (if (consp backend) backend (list backend))
-			'(:with company-yasnippet))))
-(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
-
 (defun exand-yasnippet-from-keyword (keyword)
   (interactive)
   (indent-according-to-mode)
