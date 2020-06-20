0148f85bec27ea1024ffc6c2285e4bf9ca872fdd
Sharkz Wang <sharkz.wang@gmail.com>
2020-06-21 00:22:30 +0800

    ycmd: updated comments

--- lisp/init-ycmd.el
+++ lisp/init-ycmd.el
@@ -1,3 +1,6 @@
+;; note: why choosed ycmd: subsequnce completion
+;; minor drawback: a lot of garbage entries (e.g., macros)
+
 (require-package 'ycmd)
 (require-package 'company-ycmd)
 
@@ -6,11 +9,14 @@
 ;; 2) git submodule update --init --recursive
 ;; 3) install mono-xbuild and mono-devel
 ;; 4) ./build.py --clang-completer --omnisharp-completer --gocode-completer --system-libclang
+;; 5) appending search path into `.ycm_extra_conf.py'
+;;    for example,
+;;    [ "-system",
+;;      "/usr/include/stdio.h" ]
 
 (add-hook 'company-mode-hook (lambda ()
-			       (company-ycmd-setup)
-			       (global-ycmd-mode)
-			       ))
+	    (company-ycmd-setup)
+	    (global-ycmd-mode)))
 
 (setq ycmd-force-semantic-completion t)
 (set-variable 'ycmd-server-command (list "python" (expand-file-name "/opt/ycmd/ycmd")))
