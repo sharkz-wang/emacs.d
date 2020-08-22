eab362400a4325991a475575b6192f8abd6325e5
Sharkz Wang <sharkz.wang@gmail.com>
2020-08-22 19:18:42 +0800

    helm: added key-binding to kill minibuffer lines

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -102,6 +102,7 @@
 (define-key helm-map (kbd "C-o") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-j") 'helm-next-line)
 (define-key helm-map (kbd "C-k") 'helm-previous-line)
+(define-key helm-map (kbd "C-u") 'kill-whole-line)
 (define-key helm-map (kbd "C-h") 'helm-find-files-up-one-level)
 (define-key helm-map (kbd "C-l") 'helm-find-files-down-last-level)
 
