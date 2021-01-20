c01d532b101709b44b216d9c4486ba3857575a02
Sharkz Wang <sharkz.wang@gmail.com>
2021-01-20 22:32:26 +0800

    cust-var: updated default values

--- lisp/cust-vars.el
+++ lisp/cust-vars.el
@@ -1,10 +1,11 @@
 (custom-set-variables
+ '(kernel-src-repo-list
+   '("~/src/linux"))
  '(org-agenda-dir (expand-file-name "~/org"))
  '(org-snapshot-dir (expand-file-name "~/org/snapshots"))
  '(org-plantuml-jar-path (expand-file-name "~/plantuml.jar"))
  '(plantuml-jar-path (expand-file-name "~/plantuml.jar"))
  '(pdf-info-epdfinfo-program "~/epdfinfo")
- '(sublimity-attractive-centering-width 90)
  '(ycmd-server-command (list "python" (expand-file-name "~/ycmd/ycmd")))
  '(ycmd-global-config "~/ycmd/cpp/ycm/.ycm_extra_conf.py")
  )
