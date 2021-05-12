49522d4964c0bfbda2cc32ba000234d92e4275fb
Sharkz Wang <sharkz.wang@gmail.com>
2021-05-12 19:26:05 +0800

    company: enhanced hydra-insert-menu

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -33,14 +33,43 @@
 
 (defhydra hydra-insert-menu (:color pink :hint nil :exit t)
   "
-^Insert^
-^^^^^^^^-------------------------
-_i_: yasnippet
+^Snippets^           ^Comment^         ^Argument^
+---------------------------------------------------
+_i_: from menu       _T_: TODO         _a_: append
+_f_: for-loop        _F_: FIXME        _s_: select
+_p_: print           _X_: XXX
 "
   ("i" helm-yas-complete)
 
+  ("f" (funcall insert-for-loop))
+  ("p" (funcall insert-print))
+
+  ("T" (funcall insert-todo-comment))
+  ("F" (funcall insert-fixme-comment))
+  ("X" (funcall insert-xxx-comment))
+
+  ("a" (funcall append-argument))
+  ("s" (funcall insert-argument-select))
+
   ("c" nil "cancel" :color blue)
   )
+
+(setq-default insert-for-loop 'nil)
+(make-variable-buffer-local 'insert-for-loop)
+(setq-default insert-print 'nil)
+(make-variable-buffer-local 'insert-print)
+
+(setq-default insert-todo-comment 'nil)
+(make-variable-buffer-local 'insert-todo-comment)
+(setq-default insert-fixme-comment 'nil)
+(make-variable-buffer-local 'insert-fixme-comment)
+(setq-default insert-xxx-comment 'nil)
+(make-variable-buffer-local 'insert-xxx-comment)
+
+(setq-default append-argument 'nil)
+(make-variable-buffer-local 'append-argument)
+(setq-default insert-argument-select 'nil)
+(make-variable-buffer-local 'insert-argument-select)
 
 (global-set-key (kbd "C-c i") 'hydra-insert-menu/body)
 (evil-global-set-key 'normal (kbd "SPC i") 'hydra-insert-menu/body)
