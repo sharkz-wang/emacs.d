f831cedadac696b7fa3af64590399d03e155f06f
Sharkz Wang <sharkz.wang@gmail.com>
2021-05-15 12:27:44 +0800

    bookmarked-repo: enhanced directory structure

--- lisp/init-bookmarked-repos.el
+++ lisp/init-bookmarked-repos.el
@@ -7,15 +7,56 @@
 
 (defhydra hydra-bookmarked-repo-menu (:color pink :exit t)
   "
-^Search^
+^Bookmarked locations^
 ^^^^^^^^-----------------------------------------------------------------
 "
   ("e" (bookmarked-repo-do-action "~/.emacs.d") "~/.emacs.d\n")
   ("l" (bookmarked-repo-do-action "~/.emacs.d/lisp") "~/.emacs.d/lisp\n")
   ("L" (bookmarked-repo-do-action "~/.emacs.d/elpa") "~/.emacs.d/elpa\n")
-  ("k" (bookmarked-repo-do-action "~/src/linux/linux-5.4.81") "kernel\n")
-  ("i" (bookmarked-repo-do-action "~/src/linux/linux-5.4.81/include") "kernel/include\n")
-  ("d" (bookmarked-repo-do-action "~/src/linux/linux-5.4.81/Documentation") "kernel/Documentation\n")
+  ("k" hydra-kernel-repo-menu/body "kernel\n")
+
+  )
+
+(defhydra hydra-kernel-repo-menu (:color pink :exit t)
+  "
+^Kernel codebase^
+^^^^^^^^-----------------------------------------------------------------
+"
+  ("k" (bookmarked-repo-do-action "~/linux")
+       "kernel\n")
+  ("i" (bookmarked-repo-do-action "~/linux/include")
+       "kernel/include\n")
+  ("d" (bookmarked-repo-do-action "~/linux/Documentation")
+       "kernel/Documentation\n")
+
+  ("s" hydra-kernel-scheduler-menu/body "scheduler\n")
+  ("p" hydra-kernel-perf-events-menu/body "perf_events\n")
+
+  ("q" nil "quit" :color blue)
+  )
+
+(defhydra hydra-kernel-scheduler-menu (:color pink :exit t)
+  "
+^Scheduler source files^
+^^^^^^^^-----------------------------------------------------------------
+"
+  ("s" (bookmarked-repo-do-action "~/linux/kernel/sched")
+       "subsystem folder\n")
+  ("f" (bookmarked-repo-do-action "~/linux/kernel/sched/fair.c")
+       "fcs scheduler\n")
+  )
+
+(defhydra hydra-kernel-perf-events-menu (:color pink :exit t)
+  "
+^Perf_events source files^
+^^^^^^^^-----------------------------------------------------------------
+"
+  ("a" (bookmarked-repo-do-action "~/linux/kernel/events/core.c")
+       "framework layer\n")
+  ("s" (bookmarked-repo-do-action "~/linux/drivers/perf/arm_pmu.c")
+       "ARM family hal layer\n")
+  ("d" (bookmarked-repo-do-action "~/linux/arch/arm64/kernel/perf_event.c")
+       "ARMv8 driver\n")
 
   ("q" nil "quit" :color blue)
   )
