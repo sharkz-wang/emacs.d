d4d06363ac409325e1ab9c28d620474ef8d3845e
Sharkz Wang <sharkz.wang@gmail.com>
2021-05-16 18:04:46 +0800

    staging: added generation of elixir bootlin url

--- lisp/staging.el
+++ lisp/staging.el
@@ -1,3 +1,10 @@
 ;; place experimental code here
 
+;; TODO: programatically get tag name
+(defun elixir-bootlin-get-url (rev rel-path line)
+  (interactive)
+  (format "https://elixir.bootlin.com/linux/v4.9.92/source/%s#L%s"
+	  rel-path
+	  line))
+
 (provide 'staging)
