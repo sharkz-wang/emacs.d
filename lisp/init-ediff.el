5909bba2f9f12d1039508dbac31538f637351051
Sharkz Wang <sharkz.wang@gmail.com>
2021-05-16 20:37:05 +0800

    ediff: differentiated copying a+b/b+a to buffer c

--- lisp/init-ediff.el
+++ lisp/init-ediff.el
@@ -1,17 +1,40 @@
-(defun ediff-copy-both-to-C ()
+(defun ediff-copy-both-to-C (head buttom)
   (interactive)
   (ediff-copy-diff
-   ediff-current-difference nil 'C nil
-   (concat
-    (ediff-get-region-contents ediff-current-difference
-			       'A ediff-control-buffer)
-    (ediff-get-region-contents ediff-current-difference
-			       'B ediff-control-buffer))))
+      ediff-current-difference
+      nil
+      'C
+      nil
+      (concat
+          (ediff-get-region-contents
+              ediff-current-difference
+              head
+              ediff-control-buffer)
+          (ediff-get-region-contents
+              ediff-current-difference
+              buttom
+              ediff-control-buffer))))
 
-(defun init-ediff ()
-  (define-key ediff-mode-map "d" 'ediff-copy-both-to-C)
-  )
 
-(add-hook 'ediff-keymap-setup-hook 'init-ediff)
+;; TODO: fix following failed key-binding
+;; error message:
+;;     init-ediff: Key sequence "d a" starts with non-prefix key d
+;; code:
+;;     (define-key ediff-mode-map
+;;                 (kbd "d a")
+;;                 (lambda ()
+;;                 (interactive)
+;;                 (ediff-copy-both-to-C 'A 'B)))
+;;     (add-hook 'ediff-keymap-setup-hook 'init-ediff)
+
+(defun ediff-copy-A-B-to-C ()
+    (interactive)
+    (ediff-copy-both-to-C 'A 'B)
+)
+
+(defun ediff-copy-B-A-to-C ()
+    (interactive)
+    (ediff-copy-both-to-C 'B 'A)
+)
 
 (provide 'init-ediff)
