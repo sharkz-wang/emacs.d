489a62709cd39e72b4ab49a133a4ab8752378b12
Sharkz Wang <sharkz.wang@gmail.com>
2021-07-03 16:35:24 +0800

    compile: added keybinding to kill compilation buffer

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -5,15 +5,17 @@
   "
 ^Compile^               ^Jump to...^          ^Compilation window^
 --------------------------------------------------------------------
-_c_: compile            _j_: next error       _w_: open
-_r_: recompile          _k_: previous error   _x_: close
+_c_: compile            _n_: next error       _w_: open
+_r_: recompile          _p_: previous error   _x_: close
+_k_: kill compilation
 _e_: edit registers
 "
   ("c" run-compile-command-of-register)
   ("r" recompile)
+  ("k" kill-compilation)
   ("e" set-and-run-register-compile-command)
-  ("j" next-error)
-  ("k" previous-error)
+  ("n" next-error)
+  ("p" previous-error)
   ("w" open-compilation-window)
   ("x" delete-compilation-window)
 
