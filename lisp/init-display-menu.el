7c6a2c8d465b5753cb7048feee91579c85a70ef7
Sharkz Wang <sharkz.wang@gmail.com>
2021-08-07 13:43:33 +0800

    appearance: change keybindings for adjusting font size

--- lisp/init-display-menu.el
+++ lisp/init-display-menu.el
@@ -11,17 +11,17 @@ _p_: zoom out frame    _k_: zoom out buffer
 _0_: reset zoom
 "
   ;; zoom frame ...
-  ("n" zoom-in)
-  ("p" zoom-out)
+  ("p" zoom-in)
+  ("n" zoom-out)
 
   ;; zoom buffer ...
-  ("j" (text-scale-adjust +1))
-  ("k" (text-scale-adjust -1))
+  ("k" (text-scale-adjust +1))
+  ("j" (text-scale-adjust -1))
 
   ("0" --reset-all-zoom)
 
   ("q"   nil "cancel" :color blue)
-  )
+)
 
 (evil-global-set-key 'normal (kbd "SPC z") 'hydra-display-menu/body)
 (evil-define-minor-mode-key 'normal 'dired-mode-map
