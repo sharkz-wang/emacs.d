57358a0323f2d660fb560ad45d791f5d1f2834cb
Sharkz Wang <sharkz.wang@gmail.com>
2023-05-20 12:48:02 +0800

    gtags: add keybinding for rebuilding tag

--- lisp/init-gtags.el
+++ lisp/init-gtags.el
@@ -42,6 +42,8 @@
 	    (define-key helm-gtags-mode-map (kbd "C-c g t") 'helm-gtags-find-tag)
 	    (define-key evil-normal-state-map (kbd "SPC g t") 'helm-gtags-find-tag)
 
+	    (define-key evil-normal-state-map (kbd "SPC g R") 'rebuild-gtags-tags)
+
 	    (defun helm-gtags-dwim-new-horizontal-split () (interactive)
 		   (split-window-below)
 		   (other-window 1)
@@ -63,5 +65,34 @@
 	    (define-key evil-normal-state-map (kbd "SPC g 3 d") 'helm-gtags-dwim-new-vertical-split)
 
 	    ))
+
+(defun delete-gtags-tags ()
+  (interactive)
+  ;; code ported from `helm-gtags-clear-cache'
+  (let* ((tag-location (or helm-gtags--real-tag-location
+			   helm-gtags--tag-location))
+	 (gtags-path (concat tag-location "GTAGS"))
+	 (grtags-path (concat tag-location "GRTAGS"))
+	 (gpath-path (concat tag-location "GPATH")))
+    (delete-file gtags-path)
+    (delete-file grtags-path)
+    (delete-file gpath-path)
+    )
+  )
+
+(defun rebuild-gtags-tags ()
+  (interactive)
+  (let* ((tag-location (or helm-gtags--real-tag-location
+			   helm-gtags--tag-location))
+	 (tagroot tag-location)
+	 (label (helm-gtags--read-gtagslabel))
+	 (default-directory tagroot)
+	 (label-opt (helm-gtags--label-option label)))
+    (delete-gtags-tags)
+    ;; code ported from `helm-gtags--find-tag-simple'
+    (message "gtags is generating tags....")
+    (unless (zerop (process-file "gtags" nil nil nil "-q" label-opt))
+      (error "Failed: 'gtags -q %s'" label-opt)))
+  )
 
 (provide 'init-gtags)
