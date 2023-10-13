93a7a987e8f3c4d32fbc7a389f498184e96a2527
Sharkz Wang <sharkz.wang@gmail.com>
2023-10-14 00:54:04 +0800

    cust-var: update default width of center window

--- lisp/cust-vars.el
+++ lisp/cust-vars.el
@@ -7,7 +7,7 @@
 ;; (set-global-frame-font "Press Start 2P")
 ;; (set-global-frame-font "creep")
 (set-face-attribute 'default nil :height 200)
-(setq cwm-centered-window-width 80)
+(setq cwm-centered-window-width 100)
 
 (setq ebook-dir-path "~/ebooks")
 (setq linux-repo-dir-path "~/linux")
