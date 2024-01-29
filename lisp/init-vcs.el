ba444264a547f7402ac23b2d7bfbf4ad4eed16a9
Sharkz Wang <sharkz.wang@gmail.com>
2024-01-29 22:03:16 +0800

    vcs: replace git-gutter with diff-hl

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -1,51 +1,50 @@
-(require-package 'git-gutter)
-(require-package 'fringe-helper)
-(require 'git-gutter-fringe)
-(require-package 'git-gutter)
+(require-package 'diff-hl)
 
 (defhydra hydra-vcs-menu (:color pink :hint nil :idle 0.3)
   "
 ^Move^             ^Display^        ^Operations^
 ------------------------------------------------------
-_j_: next          _._: fringe      _r_: revert
-_k_: previous      _d_: diff        _s_: stage
+_j_: next          _d_: diff        _r_: revert
+_k_: previous                       _s_: stage
 "
-  ;; zoom ...
 
-  ("j" git-gutter:next-hunk)
-  ("k" git-gutter:previous-hunk)
-  ("." toggle-git-gutter-indicator)
-  ("d" toggle-git-gutter-popup-diff)
-  ("r" git-gutter:revert-hunk)
-  ("s" git-gutter:stage-hunk)
+  ("j" diff-hl-next-hunk)
+  ("k" diff-hl-previous-hunk)
+  ("d" diff-hl-show-hunk)
+  ("s" diff-hl-stage-current-hunk)
+  ("r" diff-hl-revert-hunk)
 
   ("q"   nil "cancel" :color blue)
 )
 
-;; TODO: move them to `init-vcs-defs.el'
-(defun toggle-git-gutter-indicator ()
-    (interactive)
-    ;; FIXME: hacky trick to force redrawing screen
-    (git-gutter:toggle)
-    (switch-to-last-buffer)
-    (switch-to-last-buffer)
-)
+(global-diff-hl-mode 1)
+(diff-hl-margin-mode 1)
 
-(defun toggle-git-gutter-popup-diff ()
-    (interactive)
-    (if (get-buffer-window "*git-gutter:diff*")
-      (delete-window (get-buffer-window "*git-gutter:diff*"))
-      (git-gutter:popup-diff)
-    )
-)
+;; don't highlight staged change
+(setq diff-hl-show-staged-changes nil)
+;; make room in display-line-number column for margin symbols
+(setq display-line-numbers-width 3)
 
-(global-git-gutter-mode 1)
+(custom-set-faces
+ '(diff-hl-margin-insert ((t :inherit line-number
+			     :foreground "#A6E22E" :weight heavy))))
+(custom-set-faces
+ '(diff-hl-margin-delete ((t :inherit line-number
+			     :foreground "#FF8700" :weight heavy))))
+(custom-set-faces
+ '(diff-hl-margin-change ((t :inherit line-number
+			     :foreground "#66D9EF" :weight heavy))))
+(custom-set-faces
+ '(diff-hl-margin-ignored ((t :inherit line-number
+			     :foreground "#747474" :weight heavy))))
+(custom-set-faces
+ '(diff-hl-margin-unknown ((t :inherit line-number
+			     :foreground "#747474" :weight heavy))))
 
-(setq git-gutter:ask-p nil)
-
-(setq git-gutter:added-sign " + ")
-(setq git-gutter:deleted-sign " - ")
-(setq git-gutter:modified-sign " * ")
+(customize-set-variable
+ 'diff-hl-margin-symbols-alist '((insert . "+") (delete . "-")
+				 (change . "*") (unknown . "?")
+				 (ignored . "i")))
 
 ;; FIXME: find a better keybinding
 (evil-global-set-key 'normal (kbd "SPC g .") 'hydra-vcs-menu/body)
