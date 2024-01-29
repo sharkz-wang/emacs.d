231cba10ea38093f11ace081925ca95ed8dcc457
Sharkz Wang <sharkz.wang@gmail.com>
2024-01-29 21:35:00 +0800

    appearance: use display-line-numbers-mode

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -157,88 +157,15 @@
 (require-package 'hl-anything)
 (global-hl-line-mode)
 
-(require-package 'linum-relative)
-
-;; set linum default to relative mode by
-;; setting `linum-format' to literal `linum-relative'.
-;; (literal `linum-relative' is used in upstream linum-relative)
-;; to set it default to absolute mode,
-;; just set `linum-format' to function `linum-format-func'.
-;; (original linum-mode does not support hl-anything,
-;;  it's an hack and not consistent)
-(setq linum-format 'linum-relative)
-
-;; use absolute line number as current symbol
-(customize-set-variable 'linum-relative-current-symbol "")
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-width 0)
 
 ;; setup highlight face
 (custom-set-faces
- '(linum-relative-current-face ((t :inherit hl-spotlight :foreground "#FF8700"))))
+ '(line-number-current-line ((t :inherit hl-spotlight
+				:foreground "#FF8700" :weight heavy))))
 
-;; advice linum-mode to remember current line number
-(defadvice linum-update (around hl-linum-update)
-		     (let ((linum-current-line-number (line-number-at-pos)))
-			       ad-do-it))
-(ad-activate 'linum-update)
-
-;; custom `linum-format' function to make vanilla linum
-;; support hl-anything
-(defun linum-format-func (line)
-  (concat
-   (propertize (format linum-format-fmt line) 'face
-			   (if (eq linum-current-line-number line)
-			   'linum-relative-current-face
-			   '((t :inherit linum))))))
-
-;; returns format strings with appropriate left align width.
-;; it returns different format string (just an inconsistency again -
-;; in linum-relative and vanilla linum, they use different
-;; printf placeholder %s and %d as numbering format) according
-;; to variable `linum-format'.
-(add-hook 'linum-before-numbering-hook
-	  (lambda ()
-	    (if (eq linum-format 'linum-format-func)
-		(setq-local linum-format-fmt
-			    (let ((w (length (number-to-string
-					      (count-lines (point-min) (point-max))))))
-			      (concat " %" (number-to-string w) "d   ")))
-	      (setq linum-relative-format
-		    (let ((w (length (number-to-string
-				      (count-lines (point-min) (point-max))))))
-		      (concat " %" (number-to-string w) "s   "))))))
-
-
-(defun toggle-linum-mode ()
-  (interactive)
-  (if (bound-and-true-p linum-mode)
-      (progn
-	(linum-mode 0)
-	)
-    (progn
-      (linum-relative-mode 1)
-      (linum-mode 1)
-      (setq left-fringe-width 0)
-      )
-    )
-  ;; force new display take effect
-  (switch-to-last-buffer)
-  (switch-to-last-buffer)
-  )
-
-(evil-leader/set-key
-  "tl" 'toggle-linum-mode
-  "tr" 'toggle-relative-linum
-  "tt" 'toggle-truncate-lines
-  )
-
-;; toggling relative mode
-(defun toggle-relative-linum ()
-  (interactive)
-  (if (eq linum-format 'linum-format-func)
-      (setq linum-format 'linum-relative)
-    (setq linum-format 'linum-format-func)
-    )
-  )
+(evil-global-set-key 'normal (kbd "SPC t l") 'global-display-line-numbers-mode)
 
 (require-package 'highlight-parentheses)
 (global-highlight-parentheses-mode 1)
